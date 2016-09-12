134d16a6b7bc59fd2cfb7179d48f3af001263506
Artem <email@email.com>
2016-09-12 18:18:32 +0300

    HW01_2

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,21 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -18,9 +27,23 @@ public class MealServlet extends HttpServlet {
     private static final Logger LOG = getLogger(MealServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("redirect to mealList");
+        LOG.debug("forward to mealList");
 
-//        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
-        response.sendRedirect("mealList.jsp");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        List<Meal> meals = Arrays.asList(
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+        );
+
+        List<MealWithExceed> filteredMealsWithExceeded = MealsUtil.getFilteredWithExceeded(meals, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
+
+        request.setAttribute("mealList", filteredMealsWithExceeded);
+        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
+//        response.sendRedirect("mealList.jsp");
     }
 }
--- src/main/webapp/mealList.jsp
+++ src/main/webapp/mealList.jsp
@@ -7,5 +7,28 @@
 <body>
 <h2><a href="index.html">Home</a></h2>
 <h2>Meal list</h2>
+<table>
+    <tr>
+        <td> DateTime</td>
+        <td> Description</td>
+        <td> Calories</td>
+    </tr>
+    <c:forEach var="meal" items="${mealList}">
+        <c:if test="${meal.exceed}">
+            <tr style="color:red">
+                <td> ${meal.dateTime} </td>
+                <td> ${meal.description} </td>
+                <td> ${meal.calories} </td>
+            </tr>
+        </c:if>
+        <c:if test="${!meal.exceed}">
+            <tr style="color:limegreen">
+                <td> ${meal.dateTime} </td>
+                <td> ${meal.description} </td>
+                <td> ${meal.calories} </td>
+            </tr>
+        </c:if>
+    </c:forEach>
+</table>
 </body>
 </html>
