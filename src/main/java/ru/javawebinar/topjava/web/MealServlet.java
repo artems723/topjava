976f02402d7d7673396b78b618d14b22a72ebe97
artem723 <email@email.com>
2016-10-07 18:31:27 +0300

    created and configured spring profiles

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.TimeUtil;
 import ru.javawebinar.topjava.web.meal.MealRestController;
@@ -30,10 +31,13 @@ public class MealServlet extends HttpServlet {
     private ConfigurableApplicationContext springContext;
     private MealRestController mealController;
 
+
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext.getEnvironment().setActiveProfiles(Profiles.ACTIVE_DB, Profiles.DATAJPA);
         mealController = springContext.getBean(MealRestController.class);
     }
 
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceJpaTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+
+/**
+ * Created by AStaver on 07.10.2016.
+ */
+@ActiveProfiles(Profiles.JPA)
+public class MealServiceJpaTest extends AbstractMealServiceTest {
+}
