02ba619487fa5f3eb1cdffb4a2da64d1a7b52805
artem723 <email@email.com>
2016-10-17 16:54:39 +0300

    migrated meal servlet to meal controller

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -6,9 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.TimeUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * User: gkislin
@@ -18,6 +28,9 @@ import javax.servlet.http.HttpServletRequest;
 public class RootController {
     @Autowired
     private UserService service;
+
+    @Autowired
+    private MealService mealService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
@@ -35,5 +48,58 @@ public class RootController {
         int userId = Integer.valueOf(request.getParameter("userId"));
         AuthorizedUser.setId(userId);
         return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.GET)
+    public String meals(Model model) {
+        int userId = AuthorizedUser.id();
+        List<MealWithExceed> mealList = MealsUtil.getWithExceeded(mealService.getAll(userId), AuthorizedUser.getCaloriesPerDay());
+        model.addAttribute("meals", mealList);
+        return "meals";
+    }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.POST)
+    public String setMeal(HttpServletRequest request) {
+        String action = request.getParameter("action");
+        int userId = AuthorizedUser.id();
+        if (action == null) {
+            final Meal meal = new Meal(
+                    LocalDateTime.parse(request.getParameter("dateTime")),
+                    request.getParameter("description"),
+                    Integer.valueOf(request.getParameter("calories")));
+
+            if (request.getParameter("id").isEmpty()) {
+                mealService.save(meal, userId);
+            } else {
+                mealService.save(meal, getId(request));
+            }
+            return "redirect:meals";
+
+        } else if ("filter".equals(action)) {
+            LocalDate startDate = TimeUtil.parseLocalDate(resetParam("startDate", request));
+            LocalDate endDate = TimeUtil.parseLocalDate(resetParam("endDate", request));
+            LocalTime startTime = TimeUtil.parseLocalTime(resetParam("startTime", request));
+            LocalTime endTime = TimeUtil.parseLocalTime(resetParam("endTime", request));
+            List<MealWithExceed> mealList = MealsUtil.getFilteredWithExceeded(
+                    mealService.getBetweenDates(startDate != null ? startDate : TimeUtil.MIN_DATE, endDate != null ? endDate : TimeUtil.MAX_DATE, userId),
+                    startTime != null ? startTime : LocalTime.MIN,
+                    endTime != null ? endTime : LocalTime.MAX,
+                    AuthorizedUser.getCaloriesPerDay()
+            );
+            request.setAttribute("meals", mealList);
+            return "meals";
+        }
+        return "redirect:meals";
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.valueOf(paramId);
+    }
+
+    private String resetParam(String param, HttpServletRequest request) {
+        String value = request.getParameter(param);
+        request.setAttribute(param, value);
+        return value;
     }
 }
